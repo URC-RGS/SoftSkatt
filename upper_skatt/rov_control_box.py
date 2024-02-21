10fe0482a67328ed1417a57c98ed24a6850e4c30
Yarik9008 <iaroslav.muhatdinov@yandex.ru>
2024-02-21 10:19:06 +0300

    update

--- upper_skatt/rov_control_box.py
+++ upper_skatt/rov_control_box.py
@@ -9,8 +9,8 @@ from pprint import pprint
 
 
 # # запуск на одноплатном пк rock
-PATH_CONFIG = '/home/rock/SoftAcademic/upperAcademic/config_control_box.json'
-PATH_LOG = '/home/rock/SoftAcademic/upperAcademic/.log/'
+PATH_CONFIG = '/home/rock/SoftSkatt/upperAcademic/config_control_box.json'
+PATH_LOG = '/home/rock/SoftSkatt/upperAcademic/.log/'
 
 # # запуск на компьютере в офисе 
 # PATH_CONFIG = 'C:/Users/Yarik/Documents/SoftAcademic/upperAcademic/config_control_box.json'
